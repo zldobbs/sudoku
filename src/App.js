41d65d3b83836396b71ea4d1ddc45531f5f38339
Zach Dobbs <dobbszach@gmail.com>
2019-03-22 19:40:24 -0500

    Initial commit, setup UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import './App.css';
+
+import Board from './Board';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container center">
+        <h1>Sudoku</h1>
+        <div className="row">
+          <div className="col s12 m6 push-m3">
+            <Board></Board>
+          </div>
+        </div>
       </div>
     );
   }
