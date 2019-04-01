b06ea7bdebafc3c24964afdf76085be03456e95d
Zach Dobbs <dobbszach@gmail.com>
2019-03-31 20:11:45 -0500

    Creating full solution

--- src/App.js
+++ src/App.js
@@ -13,7 +13,15 @@ class App extends Component {
       sects: []
     }
 
-    this.buildSolution();
+    /* 
+      the algorithm could result in an impossible to fix solution
+      if this is the case, just abandon it and rebuild
+    */
+    while (!this.buildSolution()) {
+      console.log('building solution...');
+      this.state.soln = [];
+      this.state.sects = [];
+    }
   }
 
   // shuffles the elements of the given array
@@ -32,6 +40,30 @@ class App extends Component {
     return result; 
   }
 
+  // asserts that the computed solution is valid 
+  assertCorrectSolution() {
+    console.log(this.state.soln);
+    let s = this.state.soln; 
+    let i, j, c = [], r = [];
+    // check if columns contain only 1..9 
+    for (i = 0; i < 9; i++) {
+      c = [];
+      r = [];
+      for (j = 0; j < 9; j++) {
+        if (c.indexOf(s[j][i]) >= 0)
+          return false; 
+        else 
+          c.push(s[j][i]);
+
+        if (r.indexOf(s[i][j]) >= 0)
+          return false;
+        else
+          r.push(s[i][j]);
+      }
+    }
+    return true; 
+  }
+
   /*
     puts the section into the solution
     sect  : section to fill
@@ -41,7 +73,7 @@ class App extends Component {
             6 7 8
   */
   fillSection(sect, q) {
-    console.log(sect);
+    // console.log(sect);
     let s = this.state.soln; 
     let row = Math.floor(q / 3) * 3; 
     let col = (q % 3) * 3; 
@@ -65,31 +97,44 @@ class App extends Component {
   */
   buildSection(init, q) {
     // fill two rows make sure they fit the solution and section
-    let i, j, curr, row, col; 
+    let i, j, curr, row, col, sects, switched; 
+    let placed = false; 
     let used = init.slice(); 
+    // fill init with -1's for the rest of the array
+    let h = [...Array(6)].map(x => -1);
+    init.push(...h);
     for (i = 0; i < 2; i++) {
       row = (Math.floor(q / 3) * 3) + i + 1; 
       for (j = 0; j < 3; j++) {
-        // pick a random number not yet used in this section 
-        curr = Math.floor(Math.random() * 9) + 1; 
-        while (used.indexOf(curr) >= 0) curr = Math.floor(Math.random() * 9) + 1; 
-        col = ((q % 3) * 3) + j; 
-        if (this.checkRow(row, curr) && this.checkColumn(col, curr)) {
-          init.push(curr);
+        placed = false; 
+        while (init[((i+1)*3) + j] === -1 && !placed && used.length < 9) {
+          // pick a random number not yet used in this section 
+          curr = Math.floor(Math.random() * 9) + 1; 
+          while (used.indexOf(curr) >= 0) curr = Math.floor(Math.random() * 9) + 1; 
+          col = ((q % 3) * 3) + j; 
+          if (this.checkRow(row, curr) && this.checkColumn(col, curr)) {
+            init[((i+1)*3) + j] = curr;
+            placed = true; 
+          }
+          else {
+            // need to move it around
+            switched = this.switchInSection(init, curr, q, i+1, j);
+            if (switched === -1) {
+              return false; 
+            } 
+            else if (switched === 1) {
+              placed = true; 
+            }
+          }
+          used.push(curr);
         }
-        else {
-          // need to move it around
-          // this.switchSection(); 
-          init.push(-1);
-        }
-        used.push(curr);
       }
     }
     this.fillSection(init, q); 
-    let sects; 
     sects = this.state.sects; 
     sects.push(init);
     this.state.sects = sects;
+    return true; 
   }
 
   // build the entire sudoku solution 
@@ -109,16 +154,54 @@ class App extends Component {
       k = (i % 3) * 3; 
       initialSect = init[j].slice(k, k+3); 
       // console.log(initialSect);
-      this.buildSection(initialSect, i);
+      if (!this.buildSection(initialSect, i)) {
+        return false; 
+      }
     }
+    // the solution should be finished, but just for safety assert its correct 
+    return this.assertCorrectSolution(); 
     // console.log(this.state.soln);
   }
 
-  switchInSection() {
-    // TODO 
+  /*
+    finds a new place for e in the section 
+    sect  : the section that is being modified
+    e     : the value that needs a location 
+    q     : the quadrant the section is contained in 
+    r, c  : row and column offset of where e is trying to be placed 
+  */
+  switchInSection(sect, e, q, r, c) {
+    // console.log('sect: ' + sect);
+    let i, j, row, col, t; 
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        // see if e will fit here 
+        row = (Math.floor(q / 3) * 3) + i; 
+        col = ((q % 3) * 3) + j; 
+        if (this.checkRow(row, e) && this.checkColumn(col, e)) {
+          if (sect[(i*3)+j] === -1) {
+            sect[(i*3)+j] = e; 
+            return 0; 
+          }
+          else {
+            // if something is already here, we should try to swap the elements
+            row = (Math.floor(q / 3) * 3) + r; 
+            col = ((q % 3) * 3) + c; 
+            if (this.checkRow(row, sect[(i*3)+j]) && this.checkColumn(col, sect[(i*3)+j])) {
+              // make the swap 
+              t = sect[(i*3)+j];
+              sect[(i*3)+j] = e;
+              sect[(r*3)+c] = t; 
+              return 1; 
+            }
+          }
+        }
+      }
+    }
+    return -1;  
   }
 
-
+  // check if this element is contatined in the column 
   checkColumn(col, k) {
     let s = this.state.soln.slice(); 
     let c = [];
@@ -128,6 +211,7 @@ class App extends Component {
     return c.indexOf(k) < 0 ? true : false; 
   }
 
+  // check is this element is contained in the row 
   checkRow(row, k) {
     let s = this.state.soln[row];
     return s.indexOf(k) < 0 ? true : false; 
@@ -136,6 +220,8 @@ class App extends Component {
   /* 
     generates 3 row solutions 
     these will be stored in rows 1, 4, and 7
+
+    FIXME error with first column leaving duplicates 
   */
   firstThree() {
     let soln = [];
@@ -184,7 +270,6 @@ class App extends Component {
       }
     }
 
-    console.log(soln);
     return soln;
   }
 
